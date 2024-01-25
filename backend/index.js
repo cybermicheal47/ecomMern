b7752adc06fd0aea4c097d14148cc7e228b41520
cybermicheal47 <65826184+cybermicheal47@users.noreply.github.com>
2024-01-25 00:02:09 -0800

    added origin cors

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,12 @@ dotenv.config();
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", [
+    "*",
+    "https://ecommm-t6dm.onrender.com/",
+  ]);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type", "Authorization");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   next();
 });
