ed8f122cc33880ca5b845fab8c7035ba69d4c87b
cybermicheal47 <65826184+cybermicheal47@users.noreply.github.com>
2024-01-25 00:30:07 -0800

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,17 @@ const https = require("https");
 app.use(cors());
 dotenv.config();
 
-// Enable CORS for all routes
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", [
-    "*",
-    "https://ecommm-t6dm.onrender.com/",
-  ]);
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://ecomappi.onrender.com",
+  ];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
